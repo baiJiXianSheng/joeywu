edf6dfce097d88007c30013e8a21dbadcabccef2
joeywu <wuc@aheading.com>
2019-09-10 18:14:02 +0800

    fix jsmin function, update .gitignore .babelrc

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
-import Common from  "./common";
+// import Common from  "./common";
 
-console.log("Author is:", Common.author);
+// console.log("Author is:", Common.author);
 
 document.getElementById("click-btn").onclick = evt => {
     alert("click!");
     document.getElementsByTagName("section")[0].style.opacity = 0.3;
 }
+
+var age = 20,
+    sex = "male",
+    str = `age:${age} male:${sex}`;
