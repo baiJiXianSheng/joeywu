80919046deb02e001d50e23b6a68505623ce7992
joeywu <wuc@aheading.com>
2019-09-11 15:08:41 +0800

    test gulp task

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@
 
 document.getElementById("click-btn").onclick = evt => {
     alert("click!");
-    document.getElementsByTagName("section")[0].style.opacity = 0.3;
+    document.getElementsByTagName("section")[0].style.background = "deepskyblue";
 }
 
 var age = 20,
